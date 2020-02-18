390efc1328351eab3db16a13b57500199b35b5f1
snigu <snigurigor05116@gmail.com>
2020-02-18 20:02:03 +0200

    commit 11

--- src/Redux/users-reducer.js
+++ src/Redux/users-reducer.js
@@ -6,13 +6,15 @@ const SET_USERS = 'SET_USERS';
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
 const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING';
+const TOGGLE_IS_FOLLOWING_PROGRESS = 'TOGGLE_IS_FOLLOWING_PROGRESS';
 
 let initialState = {
     users: [],
     pageSize: 5,
     totalUsersCount: 0,
     currentPage: 1,
-    isFetching: true
+    isFetching: true,
+    followingInProgress: false
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -45,6 +47,8 @@ const usersReducer = (state = initialState, action) => {
             return {...state, totalUsersCount: action.totalCount};
         case TOGGLE_IS_FETCHING:
             return {...state, isFetching: action.isFetching};
+            case TOGGLE_IS_FOLLOWING_PROGRESS:
+            return {...state, followingInProgress: action.isFetching};
         default:
             return state
     }
@@ -58,6 +62,7 @@ export const setUsers = (users) => ({type: SET_USERS, users});
 export const setCurrentPage = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage});
 export const setTotalUsersCount = (totalCount) => ({type: SET_TOTAL_USERS_COUNT, totalCount});
 export const toggleIsFetching = (isFetching) => ({type: TOGGLE_IS_FETCHING, isFetching});
+export const toggleFollowingProgress = (isFetching) => ({type: TOGGLE_IS_FOLLOWING_PROGRESS, isFetching});
 
 export const getUsers = (currentPage, pageSize) => {
     return (dispatch) => {
