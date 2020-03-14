1062b745fb1d0428f45f7d015b2f82c109eeb997
snigu <snigurigor05116@gmail.com>
2020-03-14 11:40:38 +0200

    commit 26 selectors 1

--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -4,12 +4,19 @@ import {
     follow,
     setCurrentPage,
     setUsers,
-    unfollow, getUsers
+    unfollow, requestUsers
 } from "../../Redux/users-reducer";
 import Users from "./Users";
 import Preloader from "../common/Preloader/Preloader";
 import {compose} from "redux";
 import {withAuthRedirect} from "../../hoc/withAuthRedirect";
+import {
+    getCurrentPage, getFollowingInProgress,
+    getIsFetching,
+    getPageSize,
+    getTotalUsersCount,
+    getUsers
+} from "../../Redux/selectors/users-selectors";
 
 class UsersContainer extends React.Component {
     componentDidMount() {
@@ -36,14 +43,25 @@ class UsersContainer extends React.Component {
     }
 }
 
+// let mapStateToProps = (state) => {
+//     return {
+//         users: state.usersPage.users,
+//         pageSize: state.usersPage.pageSize,
+//         totalUsersCount: state.usersPage.totalUsersCount,
+//         currentPage: state.usersPage.currentPage,
+//         isFetching: state.usersPage.isFetching,
+//         followingInProgress: state.usersPage.followingInProgress,
+//     }
+// };
+
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users,
-        pageSize: state.usersPage.pageSize,
-        totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage,
-        isFetching: state.usersPage.isFetching,
-        followingInProgress: state.usersPage.followingInProgress,
+        users: getUsers(state),
+        pageSize: getPageSize(state),
+        totalUsersCount: getTotalUsersCount(state),
+        currentPage: getCurrentPage(state),
+        isFetching: getIsFetching(state),
+        followingInProgress: getFollowingInProgress(state),
     }
 };
 
@@ -54,6 +72,6 @@ export default compose(
         unfollow,
         setUsers,
         setCurrentPage,
-        getUsers
+        getUsers: requestUsers
     })
 )(UsersContainer)
