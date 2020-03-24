d207e239d70a3fd106af92f8362ef0264ed923f4
snigu <snigurigor05116@gmail.com>
2020-03-24 10:29:26 +0200

    commit 34  chrome extensions for React & Redux

--- src/Redux/redux-store.js
+++ src/Redux/redux-store.js
@@ -1,10 +1,10 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
-import  thunkMiddleware from "redux-thunk"
+import thunkMiddleware from "redux-thunk"
 import {reducer as formReducer} from "redux-form"
 import appReducer from "./app-reducer";
 
@@ -14,10 +14,12 @@ let reducers = combineReducers({
     sidebar: sidebarReducer,
     usersPage: usersReducer,
     auth: authReducer,
-    app:appReducer,
+    app: appReducer,
     form: formReducer
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
+window.__store__ = store;
 
 export default store
